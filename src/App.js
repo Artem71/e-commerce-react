fe0b65b83d0116b040beb1e428adbe8fefdf38e6
Artem71 <fich71@mail.ru>
2021-06-22 23:43:24 +0300

    create homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HodePage from './homepage.component'
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HodePage />
+    </div>;
   }
 }
 
