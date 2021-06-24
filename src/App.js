6af4ba39fc646adc0662acc2bf91c7fcdc275fbd
Artem71 <fich71@mail.ru>
2021-06-24 23:09:16 +0300

    Добавил раутинг

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
 import './App.css';
 
-import HodePage from './pages/homepage/homepage.component.jsx'
+import HomePage from './pages/homepage/homepage.component.jsx'
+
+const HatsPage = () => (
+  <h1>Hats</h1>
+)
 
 class App extends Component {
   render() {
     return <div className='App'>
-      <HodePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>;
   }
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,7 +14,7 @@ export default class Directory extends React.Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'shop/hats',
         },
         {
           title: 'jackets',
@@ -50,13 +50,8 @@ export default class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, id, imageUrl, size }) => (
-            <MenuItem
-              key={id}
-              title={title}
-              imageUrl={imageUrl}
-              size={size}
-            />
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={id} { ...otherSectionProps } />
           ))
         }
       </div>
