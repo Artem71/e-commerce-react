8d434dffc8eee38d606da00db1296ded6f67ec05
Artem71 <fich71@mail.ru>
2021-06-27 16:38:58 +0300

    добавил компонент хедера

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx'
 
 class App extends Component {
   render() {
     return <div className='App'>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
